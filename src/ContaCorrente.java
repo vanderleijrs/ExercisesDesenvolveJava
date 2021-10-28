4ffb5796614af82fce37165ef638facdd432a314
vanderlei_jose <vanderlei_jose@sicredi.com.br>
2021-10-28 10:08:13 -0300

    Add classe de conta corrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,2 +1,20 @@
 public class ContaCorrente {
+    public ContaCorrente(int nroConta) {
+        this.nroConta = nroConta;
+    }
+    private double saldo;
+    private int nroConta;
+    public double getSaldo() {
+        return saldo;
+    }
+    public void deposito(double valor){
+        this.saldo+=valor;
+    }
+    public double retirada(double valor){
+        double novoSaldo=0.0;
+        if(getSaldo() >valor){
+            novoSaldo= getSaldo() - valor;
+        }
+        return novoSaldo;
+    }
 }
